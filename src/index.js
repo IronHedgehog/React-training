1bef05663451959ac96e5eab57e20f1352d2b297
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-11-02 12:21:57 +0200

    component/Menu

--- src/index.js
+++ src/index.js
@@ -51,9 +51,11 @@ function Pizza(props) {
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
-      <h3>{props.pizzaObj.name}</h3>
-      <p>{props.pizzaObj.ingredients}</p>
-      <span>{props.pizzaObj.price}</span>
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
     </li>
   );
 }
